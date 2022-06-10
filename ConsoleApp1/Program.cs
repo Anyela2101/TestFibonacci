295b9e266e15fcb9520b0ac7002d2d20e3b4f54d
Anyela2101 <anyelasalgado10b2015@gmail.com>
2022-06-10 00:04:19 -0500

    Eejecutar pruebas automatizadas

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,28 @@ namespace Fibonacci
 
         public static int serieFibonacci(int numero)
         {
-            return 0;
+            int num1 = 0;
+            int num2 = 1;
+
+            if (numero == 0)
+            {
+                return 0;
+            }else if (numero == 1 || numero==2)
+            {
+                return 1;
+            }
+            else
+            {
+                for (int i = 0; i < numero; i++)
+                {
+                    int temp = num1;
+                    num1 = num2;
+                    num2 = temp + num2;
+                }
+            }
+
+            
+            return num1;
         }
       
     }
--- TestFibonacci/UnitTest1.cs
+++ TestFibonacci/UnitTest1.cs
@@ -9,9 +9,35 @@ namespace TestFibonacci
         public void TestFibonacci1()
         {
             int numero = Fibonacci.Program.serieFibonacci(0);
+            Assert.AreEqual(0, numero);
+        }
+
+        [TestMethod]
+        public void TestFibonacci2()
+        {
+            int numero = Fibonacci.Program.serieFibonacci(1);
             Assert.AreEqual(1, numero);
         }
 
-       
+        [TestMethod]
+        public void TestFibonacci3()
+        {
+            int numero = Fibonacci.Program.serieFibonacci(2);
+            Assert.AreEqual(1, numero);
+        }
+
+        [TestMethod]
+        public void TestFibonacci4()
+        {
+            int numero = Fibonacci.Program.serieFibonacci(3);
+            Assert.AreEqual(2, numero);
+        }
+
+        [TestMethod]
+        public void TestFibonacci5()
+        {
+            int numero = Fibonacci.Program.serieFibonacci(100000);
+            Assert.AreEqual(873876091, numero);
+        }
     }
 }
